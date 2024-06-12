ef6dca107304cb91abf042642db39ef4262717c4
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-06-12 19:48:54 +0200

    Feat: test foreach card keys and add img

--- resources/views/cards.blade.php
+++ resources/views/cards.blade.php
@@ -2,4 +2,13 @@
 
 @section('content')
 <h2>cards</h2>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            @foreach ($cards as $curCard )
+                {{ $curCard['name'] }}
+            @endforeach
+        </div>
+    </div>
+</div>
 @endsection
