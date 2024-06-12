379305ec2c6f39ae09db395eb68577ffe5f287e5
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-06-12 20:05:14 +0200

    Feat: add card bootstrap

--- resources/views/cards.blade.php
+++ resources/views/cards.blade.php
@@ -1,14 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<h2>cards</h2>
-<div class="container">
-    <div class="row">
-        <div class="col">
-            @foreach ($cards as $curCard )
-                {{ $curCard['name'] }}
-            @endforeach
+    <h2 class="text-center">Cards</h2>
+    <div class="container py-5">
+        <div class="row ">
+            <div class="col d-flex justify-content-around gap-4 flex-wrap">
+                @foreach ($cards as $curCard)
+                    <div class="card" style="width:20rem;">
+                        <img src="{{ Vite::asset('resources/img/' . $curCard['frontImage']) }}" alt="">
+                        <div class="card-body">
+                            {{ $curCard['name'] }}
+                        </div>
+                    </div>
+                @endforeach
+            </div>
         </div>
+
     </div>
-</div>
 @endsection
